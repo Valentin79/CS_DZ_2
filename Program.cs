53f83cd22d324564d065846d200c663240117cd2
Valentin <zvezdochet.79@mail.ru>
2023-10-18 21:50:25 +0300

    sem_2+dz_2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CS_Sem_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = new Bits(7);
+            var a = "";
+            for (int j = 0; j < 8; j++)
+            {
+                a += value.GetBit(j) ? 1 : 0; // если i = true, записываем 1, иначе 0
+            }
+            a = string.Join("", a.Reverse());
+            Console.WriteLine(a);
+
+            value.SetBit(false, 2);
+            Console.WriteLine(value.Value);
+
+
+            long l = 1111111;
+            int i = 22222;
+            short s = 3333;
+            byte b = 44;
+
+            Bits res_long = l;
+            Bits res_int = i;
+            Bits res_short = s;
+            Bits res_bit = b;
+
+            Console.WriteLine($"{res_long}, {res_int}, {res_short}, {res_bit} ");
+        }
+    }
+}
